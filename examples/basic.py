b66e16caa64728b48b77e8a461b64114f2fd109b
nrahaman <nasim.rahaman@tuebingen.mpg.de>
2020-08-02 20:02:23 +0200

    - clean up the example

--- examples/basic.py
+++ examples/basic.py
@@ -4,22 +4,24 @@ from ucdp_ged import transforms as tr
 
 
 def make_basic_loader(path="data/ged201.csv", **loader_kwargs):
-    transforms = tr.Compose([
-        # Date and time
-        tr.DateToDaysSinceOrigin(),
-        tr.TimeStartEndToMidpoint(),
-        # Location
-        tr.LatLonToNVector(),
-        tr.WherePrecToSpatialDeltaDot(),
-        # Actors, dyads and conflicts
-        tr.RemapIDs(),
-        # Meta info
-        tr.RemapCategories(keys="type_of_violence"),
-        # NLP
-        tr.PruneAndSepSources(keep_num_sources=15),
-        # Convert to pytorch
-        tr.AsTensor()
-    ])
+    transforms = tr.Compose(
+        [
+            # Date and time
+            tr.DateToDaysSinceOrigin(),
+            tr.TimeStartEndToMidpoint(),
+            # Location
+            tr.LatLonToNVector(),
+            tr.WherePrecToSpatialDeltaDot(),
+            # Actors, dyads and conflicts
+            tr.RemapIDs(),
+            # Meta info
+            tr.RemapCategories(keys="type_of_violence"),
+            # NLP
+            tr.PruneAndSepSources(keep_num_sources=15),
+            # Convert to pytorch
+            tr.AsTensor(),
+        ]
+    )
     ged_dataset = GED(path, transforms=transforms)
     if not loader_kwargs:
         return ged_dataset
@@ -28,8 +30,7 @@ def make_basic_loader(path="data/ged201.csv", **loader_kwargs):
         return ged_loader
 
 
-if __name__ == '__main__':
-    ged = make_basic_loader("/Users/nrahaman/Python/ucdp-ged-pytorch/data/ged201.csv",
-                            batch_size=2)
+if __name__ == "__main__":
+    path = "/Users/nrahaman/Python/ucdp-ged-pytorch/data/ged201.csv"
+    ged = make_basic_loader(path=path, batch_size=2)
     batch = next(iter(ged))
-    pass
