a9fa5c882acbc093168c56be2665adf8e1a9937f
nrahaman <nasim.rahaman@tuebingen.mpg.de>
2020-08-14 10:13:21 +0200

    - minor refactor

--- ucdp_ged/utils.py
+++ ucdp_ged/utils.py
@@ -7,10 +7,15 @@ if TYPE_CHECKING:
     from .ged import GED
 
 
-def dump_megabatch(dataset: "GED", path: str, num_workers: int = 0):
+def fetch_megabatch(
+    dataset: "GED", path: str = None, num_workers: int = 0, dump: bool = True
+):
     loader = DataLoader(
         dataset, batch_size=len(dataset), shuffle=False, num_workers=num_workers
     )
     megabatch = next(iter(loader))
-    torch.save(megabatch, path)
-
+    if dump:
+        assert path is not None, "Need a path if `dump` is True."
+        torch.save(megabatch, path)
+    else:
+        return megabatch
